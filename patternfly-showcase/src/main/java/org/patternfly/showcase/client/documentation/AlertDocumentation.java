a17633e1d46ea1920febfa886b3fad46f339291c
Harald Pehl <harald.pehl@gmail.com>
2019-10-18 10:58:36 +0200

    Enhance alert documentation

--- patternfly-showcase/src/main/java/org/patternfly/showcase/client/documentation/AlertDocumentation.java
+++ patternfly-showcase/src/main/java/org/patternfly/showcase/client/documentation/AlertDocumentation.java
@@ -1,10 +1,14 @@
 package org.patternfly.showcase.client.documentation;
 
 import org.patternfly.client.components.Alert;
+import org.patternfly.client.components.AlertGroup;
 import org.patternfly.showcase.client.resources.Code;
 
 import static java.util.Arrays.asList;
-import static org.jboss.gwt.elemento.core.Elements.*;
+import static org.jboss.gwt.elemento.core.Elements.a;
+import static org.jboss.gwt.elemento.core.Elements.div;
+import static org.jboss.gwt.elemento.core.Elements.p;
+import static org.jboss.gwt.elemento.core.Elements.span;
 import static org.patternfly.client.components.AlertGroup.toast;
 
 class AlertDocumentation extends ComponentDocumentation {
@@ -14,7 +18,7 @@ class AlertDocumentation extends ComponentDocumentation {
                 p().textContent("Alerts are used to notify the user about a change in status or other event").get(),
                 asList(
                         new Demo("Default alert", Code.get().alertDefault().getText(),
-                                () -> div()
+                                () -> div().add(AlertGroup.embedded()
                                         .add(Alert.default_("Default alert title")
                                                 .description(span()
                                                         .add("Default alert description. ")
@@ -25,118 +29,96 @@ class AlertDocumentation extends ComponentDocumentation {
                                                 //         .add("Info alert description. ")
                                                 //         .add(a("#").textContent("This is a link")))
                                                 .closable())
-                                        .add(br())
                                         .add(Alert.default_("Default alert title")
                                                 .closable())
-                                        .add(br())
                                         .add(Alert.default_("Default alert title")
                                                 .action("Action Button", () -> toast().add(
                                                         Alert.info("Default action button clicked."))))
-                                        .add(br())
                                         .add(Alert.default_("Default alert title"))
-                                        .get()),
+                                        .element()).get()),
                         new Demo("Info alert", Code.get().alertInfo().getText(),
-                                () -> div()
+                                () -> div().add(AlertGroup.embedded()
                                         .add(Alert.info("Info alert title")
                                                 .description(span()
                                                         .add("Info alert description. ")
                                                         .add(a("#").textContent("This is a link"))
                                                         .get())
                                                 .closable())
-                                        .add(br())
                                         .add(Alert.info("Info alert title")
                                                 .closable())
-                                        .add(br())
                                         .add(Alert.info("Info alert title")
                                                 .action("Action Button", () -> toast().add(
                                                         Alert.info("Info action button clicked."))))
-                                        .add(br())
                                         .add(Alert.info("Info alert title"))
-                                        .get()),
+                                        .element()).get()),
                         new Demo("Success alert", Code.get().alertSuccess().getText(),
-                                () -> div()
+                                () -> div().add(AlertGroup.embedded()
                                         .add(Alert.success("Success alert title")
                                                 .description(span()
                                                         .add("Success alert description. ")
                                                         .add(a("#").textContent("This is a link"))
                                                         .get())
                                                 .closable())
-                                        .add(br())
                                         .add(Alert.success("Success alert title")
                                                 .closable())
-                                        .add(br())
                                         .add(Alert.success("Success alert title")
                                                 .action("Action Button", () -> toast().add(
                                                         Alert.info("Success action button clicked."))))
-                                        .add(br())
                                         .add(Alert.success("Success alert title"))
-                                        .get()),
+                                        .element()).get()),
                         new Demo("Warning alert", Code.get().alertWarning().getText(),
-                                () -> div()
+                                () -> div().add(AlertGroup.embedded()
                                         .add(Alert.warning("Warning alert title")
                                                 .description(span()
                                                         .add("Warning alert description. ")
                                                         .add(a("#").textContent("This is a link"))
                                                         .get())
                                                 .closable())
-                                        .add(br())
                                         .add(Alert.warning("Warning alert title")
                                                 .closable())
-                                        .add(br())
                                         .add(Alert.warning("Warning alert title")
                                                 .action("Action Button", () -> toast().add(
                                                         Alert.info("Warning action button clicked."))))
-                                        .add(br())
                                         .add(Alert.warning("Warning alert title"))
-                                        .get()),
+                                        .element()).get()),
                         new Demo("Danger alert", Code.get().alertDanger().getText(),
-                                () -> div()
+                                () -> div().add(AlertGroup.embedded()
                                         .add(Alert.danger("Danger alert title")
                                                 .description(span()
                                                         .add("Danger alert description. ")
                                                         .add(a("#").textContent("This is a link"))
                                                         .get())
                                                 .closable())
-                                        .add(br())
                                         .add(Alert.danger("Danger alert title")
                                                 .closable())
-                                        .add(br())
                                         .add(Alert.danger("Danger alert title")
                                                 .action("Action Button", () -> toast().add(
                                                         Alert.info("Danger action button clicked."))))
-                                        .add(br())
                                         .add(Alert.danger("Danger alert title"))
-                                        .get()),
+                                        .element()).get()),
                         new Demo("Inline alert types", Code.get().alertInline().getText(),
-                                () -> div()
+                                () -> div().add(AlertGroup.embedded()
                                         .add(Alert.default_("Default inline alert title").inline())
-                                        .add(br())
                                         .add(Alert.info("Info inline alert title").inline())
-                                        .add(br())
                                         .add(Alert.success("Success inline alert title").inline())
-                                        .add(br())
                                         .add(Alert.warning("Warning inline alert title").inline())
-                                        .add(br())
                                         .add(Alert.danger("Danger inline alert title").inline())
-                                        .get()),
+                                        .element()).get()),
                         new Demo("Inline alert variations", Code.get().alertInlineVariations().getText(),
-                                () -> div()
+                                () -> div().add(AlertGroup.embedded()
                                         .add(Alert.success("Success alert title").inline()
                                                 .description(span()
                                                         .add("Danger alert description. ")
                                                         .add(a("#").textContent("This is a link"))
                                                         .get())
                                                 .closable())
-                                        .add(br())
                                         .add(Alert.success("Success alert title").inline()
                                                 .closable())
-                                        .add(br())
                                         .add(Alert.success("Success alert title").inline()
                                                 .action("Action Button", () -> toast().add(
                                                         Alert.info("Success action button clicked."))))
-                                        .add(br())
                                         .add(Alert.success("Success alert title").inline())
-                                        .get())
+                                        .element()).get())
                 ));
     }
 }
